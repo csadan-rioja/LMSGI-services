5c6b5843933d5715ab6882e61c6d8dc1f8b3ea87
Carlos Sáenz Adán <csaenz@iescomercio.com>
2023-01-19 12:27:22 +0100

    new commit

---
+++ src/test/java/com/iescomercio/lmsgi/services/ServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iescomercio.lmsgi.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
